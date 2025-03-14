5a76b4d75eec8892c9f3c41ed81b2265fd734505
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-14 10:44:37 +0530

    [Akash singh] Add: Add UC2 of Section1 to project

--- AddressBook/Controllers/AddressBook.cs
+++ AddressBook/Controllers/AddressBook.cs
@@ -7,5 +7,43 @@ namespace AddressBook.Controllers
     [ApiController]
     public class AddressBook : ControllerBase
     {
+        /// <summary>
+        /// Fetch all Contacts
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("AddressBook")]
+
+        public Task<IActionResult> AllContacts() { }
+
+        /// <summary>
+        /// Get Contact by ID 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetById")]
+        public Task<IActionResult> GetById() { }
+
+        /// <summary>
+        /// Add new Contact
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("NewContact")]
+
+        public Task<IActionResult> AddContact() { }
+
+        /// <summary>
+        /// Update the contact
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut("UpdateContact")]
+        public Task<IActionResult> UpdateContact() { }
+
+
+        /// <summary>
+        /// Delete contact by Id
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("DeleteContact")]
+
+        public Task<IActionResult> DeleteContact() { }
     }
 }
